10e1ae0122a5cb65772e77589393e350ba3c1313
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-05-11 14:11:51 +0000

    dummy

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:urban_style/controllers/engine/engine_controller.dart';
 import 'package:urban_style/pages/home/home.dart';
 import 'package:urban_style/pages/splash/splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart'; //comment
 
 import 'controllers/checkout/checkout_controller.dart';
 
